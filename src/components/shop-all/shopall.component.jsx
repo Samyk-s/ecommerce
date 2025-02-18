8df5165026e72a19691a9c65dbb663ec8f92ce16
Samyk-s <samykbajr919@gmail.com>
2025-02-18 06:10:26 +0545

    update

--- src/components/shop-all/shopall.component.jsx
+++ src/components/shop-all/shopall.component.jsx
@@ -467,8 +467,8 @@ const ShopAll = () => {
       <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
         <div className="h-56 w-full">
           <a href="#">
-            <img className="mx-auto h-full dark:hidden" src="public\MicrosoftXboxSeriesX.jpg" alt="" />
-            <img className="mx-auto hidden h-full dark:block" src="public\MicrosoftXboxSeriesX.jpg" alt="" />
+            <img className="mx-auto h-full dark:hidden" src="MicrosoftXboxSeriesX.jpg" alt="" />
+            <img className="mx-auto hidden h-full dark:block" src="MicrosoftXboxSeriesX.jpg" alt="" />
           </a>
         </div>
 
