8af1575aeb0a121bd432cd37d311c280178f5ec5
Samyk-s <samykbajr919@gmail.com>
2025-02-20 17:02:21 +0545

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'flowbite/dist/flowbite.min.css';
+import { Analytics } from '@vercel/analytics/react';  
 import Routing from './conifg/routing';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <Analytics />
   <Routing/>
   </StrictMode>,
 )
