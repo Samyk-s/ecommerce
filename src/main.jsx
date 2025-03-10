1951d9d6d07f0998ed408dfbf3103fd1e717af4d
S'amyk Bajracharya <samykbajr919@gmail.com>
2025-03-10 11:15:04 +0545

    new

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'flowbite/dist/flowbite.min.css';
 import { Analytics } from '@vercel/analytics/react';  
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import Routing from './conifg/routing';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <Analytics />
-  <SpeedInsights/>
+
   <Routing/>
   </StrictMode>,
 )
